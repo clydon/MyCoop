96b2b9e6514cb7903b7c4b8f07baa78900f94abb
huzzeykhed <bee96257@opayq.com>
2014-04-14 21:13:45 -0400

    finally got pictures and contacts working

--- My Wentworth Coop/src/main/java/com/ccdevelopment/mywentworthcoop/FragmentPeople.java
+++ My Wentworth Coop/src/main/java/com/ccdevelopment/mywentworthcoop/FragmentPeople.java
@@ -1,27 +1,57 @@
 package com.ccdevelopment.mywentworthcoop;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FragmentPeople extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState == null){
-            // onCreate First Time
-
-        } else {
-            // onCreate Subsequent Time
-
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_people, container, false);
+        final View rootview = inflater.inflate(R.layout.fragment_people, container, false);
+
+        Button buttonsave = (Button) rootview.findViewById(R.id.btnSave);
+        Button buttonclear = (Button) rootview.findViewById(R.id.btnClear);
+        final EditText mName = (EditText) rootview.findViewById(R.id.editName);
+        final EditText mEmail = (EditText) rootview.findViewById(R.id.editEmail);
+        final EditText mPhone = (EditText) rootview.findViewById(R.id.editPhone);
+
+        buttonsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                intent.putExtra(ContactsContract.Intents.Insert.NAME, mName.getText().toString());
+                intent.putExtra(ContactsContract.Intents.Insert.EMAIL, mEmail.getText());
+                intent.putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK);
+                intent.putExtra(ContactsContract.Intents.Insert.PHONE, mPhone.getText());
+                intent.putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
+                startActivity(intent);
+            }
+        });
+
+        buttonclear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mName.setText("");
+                mEmail.setText("");
+                mPhone.setText("");
+            }
+        });
+        return rootview;
+
     }
+
 }
--- My Wentworth Coop/src/main/java/com/ccdevelopment/mywentworthcoop/FragmentPhotoVideo.java
+++ My Wentworth Coop/src/main/java/com/ccdevelopment/mywentworthcoop/FragmentPhotoVideo.java
@@ -1,32 +1,97 @@
 package com.ccdevelopment.mywentworthcoop;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.Context;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.parse.DeleteCallback;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.ProgressCallback;
+import com.parse.SaveCallback;
 
 public class FragmentPhotoVideo extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (savedInstanceState == null){
-            // onCreate First Time
-
-        } else {
-            // onCreate Subsequent Time
-
-        }
-
-    }
-
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1888;
+    Button buttoncam;
+    ImageView imgView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photovideo, container, false);
+        final View rootview = inflater.inflate(R.layout.fragment_photovideo, container, false);
 
+        buttoncam = (Button) rootview.findViewById(R.id.btnCamera);
+        imgView = (ImageView) rootview.findViewById(R.id.imgPreview);
+        buttoncam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,
+                        CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        });
+        return rootview;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                //Store Picture
+                ParseFile picture = new ParseFile("Picture", byteArray);
+                picture.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(getActivity(), "Picture Uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Convert ByteArray to Bitmap::
+                Bitmap myimg = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                imgView.setImageBitmap(myimg);
+
+                //Create Parse Object for reference
+                ParseObject picob = new ParseObject("Picture");
+                picob.put("Title", "cooppic");
+                picob.put("Author", ParseUser.getCurrentUser());
+                picob.put("username", ParseUser.getCurrentUser().getUsername());
+                picob.put("Photo", picture);
+                picob.saveInBackground();
+            }
+        }
+    }
 }
