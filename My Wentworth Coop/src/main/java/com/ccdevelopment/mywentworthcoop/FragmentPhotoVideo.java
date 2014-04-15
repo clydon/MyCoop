746202137f809b35a80e83b628e7ad0f531fa3f8
clydon <lydonc@wit.edu>
2014-04-15 02:02:31 -0400

    Fixed Photo fragment to display user submitted data. Custom AlertDialog added for adding photos. Other bug fixes todo: async data in Photo fragment

--- My Wentworth Coop/src/main/java/com/ccdevelopment/mywentworthcoop/FragmentPhotoVideo.java
+++ My Wentworth Coop/src/main/java/com/ccdevelopment/mywentworthcoop/FragmentPhotoVideo.java
@@ -1,56 +1,116 @@
 package com.ccdevelopment.mywentworthcoop;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
-import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Context;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import com.parse.DeleteCallback;
-import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseImageView;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
-import com.parse.ProgressCallback;
 import com.parse.SaveCallback;
 
 public class FragmentPhotoVideo extends Fragment {
 
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1888;
-    Button buttoncam;
-    ImageView imgView;
+    View rootView, dialog_layout;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    byte[] takenPhoto;
+    Bitmap takenPhotoBmp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View rootview = inflater.inflate(R.layout.fragment_photovideo, container, false);
+        rootView = inflater.inflate(R.layout.fragment_photovideo, container, false);
 
-        buttoncam = (Button) rootview.findViewById(R.id.btnCamera);
-        imgView = (ImageView) rootview.findViewById(R.id.imgPreview);
-        buttoncam.setOnClickListener(new View.OnClickListener() {
+        ParseQueryAdapter.QueryFactory<ParseObject> factory =
+                new ParseQueryAdapter.QueryFactory<ParseObject>() {
+                    public ParseQuery create() {
+                        ParseQuery query = new ParseQuery("Picture");
+                        query.orderByDescending("createdAt");
+                        return query;
+                    }
+                };
+
+        MyAdapter adapter = new MyAdapter(getActivity(), factory);
+
+        adapter.setTextKey("Title");
+        adapter.setImageKey("Photo");
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listViewPhotos);
+        listView.setAdapter(adapter);
+
+        Button addNew = (Button) rootView.findViewById(R.id.buttonAddNew);
+        addNew.setOnClickListener(new View.OnClickListener(){
+            @Override
+        public void onClick(View v) {
+                openNewPhotoDialog();
+            }
+        });
+
+        return rootView;
+    }
+
+   private class MyAdapter extends ParseQueryAdapter<ParseObject> {
+        private Context context;
+
+        public MyAdapter(
+                Context context,
+                com.parse.ParseQueryAdapter.QueryFactory<ParseObject> queryFactory) {
+            super(context, queryFactory);
+            this.context = context;
+        }
+
+        @Override
+        public View getItemView(ParseObject object, View v, ViewGroup parent) {
+            if (v == null) {
+                v = View.inflate(this.context, R.layout.photo_row_item, null);
+            }
+            TextView titleView = (TextView) v.findViewById(R.id.textViewTitle);
+            titleView.setText(object.get("Title").toString());
+
+            TextView dateView = (TextView) v.findViewById(R.id.textViewDate);
+            dateView.setText(new SimpleDateFormat("MMM d, h:mm").format(object.getUpdatedAt()));
+
+            TextView authorView = (TextView) v.findViewById(R.id.textViewAuthor);
+            authorView.setText(object.get("username").toString());
+
+            ParseImageView imageView = (ParseImageView)v.findViewById(R.id.imageView);
+            ParseFile file = object.getParseFile("Photo");
+            if(file != null) {
+                imageView.setParseFile(file);
+                imageView.loadInBackground();
+            }
+            return v;
+        }
+    }
+
+    private void openNewPhotoDialog() {
+        dialog_layout = getActivity().getLayoutInflater().inflate(R.layout.dialog_photo, null);
+
+        final EditText editText = (EditText) dialog_layout.findViewById(R.id.editTextTitle);
+        Button addPhoto = (Button) dialog_layout.findViewById(R.id.buttonTakePhoto);
+        addPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -58,7 +118,25 @@ public class FragmentPhotoVideo extends Fragment {
                         CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
             }
         });
-        return rootview;
+
+        final AlertDialog.Builder db = new AlertDialog.Builder(getActivity());
+        db.setView(dialog_layout);
+        db.setTitle("Add New Photo");
+        db.setPositiveButton("SAVE", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(takenPhoto != null){
+                            saveScaledPhoto(takenPhoto, String.valueOf(editText.getText()));
+                        }
+                    }
+                });
+        db.setNegativeButton("CANCEL", new
+                DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        db.show();
     }
 
     @Override
@@ -71,27 +149,51 @@ public class FragmentPhotoVideo extends Fragment {
                 bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
                 byte[] byteArray = stream.toByteArray();
 
-                //Store Picture
-                ParseFile picture = new ParseFile("Picture", byteArray);
-                picture.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        Toast.makeText(getActivity(), "Picture Uploaded", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                takenPhoto = byteArray;
+                takenPhotoBmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
 
-                // Convert ByteArray to Bitmap::
-                Bitmap myimg = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-                imgView.setImageBitmap(myimg);
-
-                //Create Parse Object for reference
-                ParseObject picob = new ParseObject("Picture");
-                picob.put("Title", "cooppic");
-                picob.put("Author", ParseUser.getCurrentUser());
-                picob.put("username", ParseUser.getCurrentUser().getUsername());
-                picob.put("Photo", picture);
-                picob.saveInBackground();
+                ImageView imageView = (ImageView) dialog_layout.findViewById(R.id.imagePreview);
+                imageView.setImageBitmap(takenPhotoBmp);
             }
         }
+    }
+
+    private void saveScaledPhoto(final byte[] data, final String title) {
+        Bitmap mealImage = BitmapFactory.decodeByteArray(data, 0, data.length);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        mealImage.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+
+        byte[] scaledData = bos.toByteArray();
+
+        final ParseFile photoFile = new ParseFile("coop_picture.jpg", scaledData);
+        photoFile.saveInBackground(new SaveCallback() {
+
+            public void done(ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getActivity(),
+                            "Error saving: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    ParseObject picObject = new ParseObject("Picture");
+                    picObject.put("Title", title);
+                    picObject.put("Author", ParseUser.getCurrentUser());
+                    picObject.put("username", ParseUser.getCurrentUser().getUsername());
+                    picObject.put("Photo", photoFile);
+                    picObject.saveInBackground(new SaveCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                getActivity().finish();
+                                Intent myIntent = new Intent(getActivity(), ViewPagerActivity.class);
+                                myIntent.putExtra("FirstTab", 0);
+                                startActivity(myIntent);
+                            } else {
+                                Toast.makeText(getActivity(),"Something went wrong!",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
